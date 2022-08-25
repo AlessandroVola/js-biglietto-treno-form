93ecb9c99e77323517ee78772810f6cbfac8764e
AlessandroVola <volaalessandro@gmail.com>
2022-08-25 10:19:34 +0200

    correzioni

--- js/script.js
+++ js/script.js
@@ -1,31 +1,48 @@
 
+const ticketHTMLbutton = document.getElementById("button-create")
+// click su genera
 
-var kmDaPercorrere = document.getElementById("km")
-var nameSurname = document.getElementById("nameSurname")
-
-
-
-
-const prezzoAlKm = 0.21;
-var prezzoBigliettoNormale = kmDaPercorrere * prezzoAlKm;
-
+ticketHTMLbutton.addEventListener('click', function () {
+    // km che deve fare l'utente
+    var inputKmDaPercorrere = document.getElementById("km");
+    console.log(inputKmDaPercorrere.value);
+    var kmDaPercorrere = parseInt(inputKmDaPercorrere.value);
 
 
+    // età utente
+    const inputEta = document.getElementById("age");
+    const age = (inputEta.value)
+
+
+    // nome e cognome utente
+    var nameSurname = document.getElementById("nameSurname");
 
 
 
-// // sconto 20% minorenni e 40% over 65
-// if (!isNaN(kmDaPercorrere) && !isNaN(etaUtente)) {
-//     if (etaUtente < 18) {
-//         document.getElementById("biglietto_treno").innerHTML = "Il tuo biglietto costa " + ((prezzoBigliettoNormale / 100) * 80).toFixed(2) + " euro"
-//     }
-//     else if (etaUtente > 65) {
-//         document.getElementById("biglietto_treno").innerHTML = "Il tuo biglietto costa " + ((prezzoBigliettoNormale / 100) * 60).toFixed(2) + " euro"
-//     }
-//     else {
-//         document.getElementById("biglietto_treno").innerHTML = "Il tuo biglietto costa " + prezzoBigliettoNormale.toFixed(2) + " euro"
-//     }
-// }
-// else {
-//     document.getElementById("biglietto_treno").innerHTML = "Dati inseriti sbagliati"
-// }
+
+    const prezzoAlKm = 0.21;
+    var prezzoBigliettoNormale = kmDaPercorrere * prezzoAlKm;
+
+    console.log(prezzoBigliettoNormale)
+
+
+
+
+
+
+    // sconto 20% minorenni e 40% over 65
+    if (!isNaN(kmDaPercorrere) && !isNaN(age)) {
+        if (age = "minorenne") {
+            document.getElementById("biglietto_treno").innerHTML = "Il tuo biglietto costa " + ((prezzoBigliettoNormale / 100) * 80).toFixed(2) + " euro"
+        }
+        else if (age = "over 65") {
+            document.getElementById("biglietto_treno").innerHTML = "Il tuo biglietto costa " + ((prezzoBigliettoNormale / 100) * 60).toFixed(2) + " euro"
+        }
+        else {
+            document.getElementById("biglietto_treno").innerHTML = "Il tuo biglietto costa " + prezzoBigliettoNormale.toFixed(2) + " euro"
+            //     }
+        }
+
+    }
+})
+
